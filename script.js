80343055b102cd9a06dd1c261c3234f5db77839a
Sukhvsin2 <sukhvsingh2026@gmail.com>
2021-02-11 17:40:32 -0600

    Basic Done

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ const apiEnd = 'https://api.pokemontcg.io/v1/cards?name=charizard';
 (async function pokedexApi() {
     var res = await fetch(apiEnd);
     res = await res.json()
-    res.forEach(card => {
+    // console.log(res);
+    res.cards.forEach(card => {
         // code
-        
+        let image = document.createElement('img')
+        image.src = card.imageUrl;
+        image.alt = card.name;
+        image.className = 'apiImage'
+        container.appendChild(image);
     })
 })()
