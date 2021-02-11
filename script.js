549a5baa67763d1ab9a0e573a9d1dc6bb0fc3083
Sukhvsin2 <sukhvsingh2026@gmail.com>
2021-02-11 17:31:50 -0600

    API running

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+const container = document.querySelector('.container')
 
-function pokedexApi() {
-    
-}
+const apiEnd = 'https://api.pokemontcg.io/v1/cards?name=charizard';
+
+(async function pokedexApi() {
+    var res = await fetch(apiEnd);
+    res = await res.json()
+    res.forEach(card => {
+        // code
+        
+    })
+})()
