d3c4cba593a33cce8cf8723a6b8d84e461d3746c
Sukhvsin2 <sukhvsingh2026@gmail.com>
2021-02-11 18:08:19 -0600

    Input Running

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
 const container = document.querySelector('.container')
+const searchInput = document.querySelector('#searchComponent')
 
 const apiEnd = 'https://api.pokemontcg.io/v1/cards?name=charizard';
+var apiEndSearch = 'https://api.pokemontcg.io/v1/cards?name=';
 
-(async function pokedexApi() {
+pokedexApi();
+
+async function pokedexApi() {
     var res = await fetch(apiEnd);
     res = await res.json()
-    // console.log(res);
     res.cards.forEach(card => {
-        // code
         let image = document.createElement('img')
         image.src = card.imageUrl;
         image.alt = card.name;
         image.className = 'apiImage'
         container.appendChild(image);
     })
-})()
+}
+
+function clearScreen() {
+    document.querySelectorAll('.apiImage').forEach(e => e.remove());
+}
+
+async function searchPokemon() {
+    let pokemon = searchInput.value;
+    if (pokemon.length > 3) {
+        clearScreen();
+        var res = await fetch(apiEndSearch + pokemon)
+        res = await res.json();
+    } else if(pokemon.length == 0) {
+        pokedexApi();
+    }
+}
